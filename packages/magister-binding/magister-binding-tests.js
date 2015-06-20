a77830f53cb4c66fcc6db48157fa1f2390bffef2
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-06-20 20:09:36 +0200

    add simple tests

--- packages/magister-binding/magister-binding-tests.js
+++ packages/magister-binding/magister-binding-tests.js
@@ -1,5 +1,12 @@
-// Write your tests here!
-// Here is an example.
-Tinytest.add('example', function (test) {
-  test.equal(true, true);
+Tinytest.add('expose an object', function (test) {
+	test.instanceOf(MagisterBinding, Object);
+});
+
+Tinytest.add('have the required functions', function (test) {
+	test.isNotNull(MagisterBinding.createData);
+	test.isNotNull(MagisterBinding.getCalendarItems);
+	test.isNotNull(MagisterBinding.getClasses);
+	test.isNotNull(MagisterBinding.getGrades);
+	test.isNotNull(MagisterBinding.getPersons);
+	test.isNotNull(MagisterBinding.getStudyUtils);
 });
--- packages/magister-binding/package.js
+++ packages/magister-binding/package.js
@@ -23,5 +23,5 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('magister-binding');
-  api.addFiles('magister-binding-tests.js');
+  api.addFiles('magister-binding-tests.js', 'server');
 });
