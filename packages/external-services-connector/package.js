1865e3b07e38a34512ce6d920b18478ca8b041cf
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-12-24 13:15:33 +0100

    refactor external-services-connector package to multiple files
    This also removes the possibility for users to give another user's id as
    parameter to the Meteor methods.

--- packages/external-services-connector/package.js
+++ packages/external-services-connector/package.js
@@ -7,7 +7,7 @@ Package.describe({
 	git: '',
 	// By default, Meteor will default to using README.md for documentation.
 	// To avoid submitting documentation, set this field to null.
-	documentation: 'README.md'
+	documentation: 'README.md',
 });
 
 Package.onUse(function(api) {
@@ -18,7 +18,12 @@ Package.onUse(function(api) {
 		'check',
 		'ejson',
 	], 'server');
-	api.addFiles('external-services-connector.coffee', 'server');
+	api.addFiles([
+		'server/connector.coffee',
+		'server/functions.coffee',
+		'server/methods.coffee',
+		'server/publish.coffee',
+	], 'server');
 	api.export([
 		'ExternalServicesConnector',
 		'Services',
