ef0ce6f60e432eef0b997699594a0b368ad80a6c
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-12-25 19:42:56 +0100

    update submodules to latest commit in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git submodule init
-git submodule update --recursive ./
+git submodule update --recursive --remote --merge ./
 base="MAIL_URL='smtp://lieuwerooijakkers@gmail.com:gV1afFmrhN7IUG7VS-x89w@smtp.mandrillapp.com:587' DISABLE_WEBSOCKETS=true meteor"
 if [[ -f settings.json ]]; then
 	eval "$base --settings settings.json"
