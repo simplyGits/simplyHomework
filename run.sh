fc178662eeeb3c8983f2812abf27298031c949b7
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-01-07 16:07:16 +0100

    run.sh: remove force-ssl on run and add it back on exit

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
 git submodule init
 git submodule update --recursive --remote --merge ./
 base="MAIL_URL='smtp://lieuwerooijakkers@gmail.com:gV1afFmrhN7IUG7VS-x89w@smtp.mandrillapp.com:587' DISABLE_WEBSOCKETS=true meteor"
+meteor remove force-ssl
+trap "meteor add force-ssl" EXIT
 if [[ -f settings.json ]]; then
 	eval "$base --settings settings.json"
 else
