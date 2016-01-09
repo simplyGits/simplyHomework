f6b2d2aa75b47eca1a514c2ced186953f1cf904c
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-01-09 22:17:08 +0100

    run.sh: update smtp password

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 git submodule init
 git submodule update --recursive --remote --merge ./
-base="MAIL_URL='smtp://lieuwerooijakkers@gmail.com:gV1afFmrhN7IUG7VS-x89w@smtp.mandrillapp.com:587' DISABLE_WEBSOCKETS=true meteor"
+base="MAIL_URL='smtp://lieuwerooijakkers@gmail.com:3_oSTg-KlPmYICxcGivZMg@smtp.mandrillapp.com:587' DISABLE_WEBSOCKETS=true meteor"
 if [[ -f settings.json ]]; then
 	eval "$base --settings settings.json"
 else
