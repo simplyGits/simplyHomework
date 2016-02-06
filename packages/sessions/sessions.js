c6e31169d63a26974428baab5341e82e14d95600
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-02-06 18:23:34 +0100

    sessions

---
+++ packages/sessions/sessions.js
@@ -0,0 +1,41 @@
+const sessions = new Mongo.Collection('sessions')
+const currentSessionId = new ReactiveVar()
+
+Tracker.autorun(function () {
+	Meteor.connection._userIdDeps.depend()
+	Meteor.defer(function () {
+		const token = localStorage.getItem('Meteor.loginToken')
+		if (token != null) {
+			Meteor.call(
+				'sessions_extend',
+				token,
+				function (e, r) {
+					currentSessionId.set(r)
+				}
+			)
+		}
+	})
+})
+
+Tracker.autorun(function () {
+	if (Meteor.userId() != null) {
+		Meteor.subscribe('sessions')
+	}
+})
+
+Sessions = {
+	all() {
+		return sessions.find().fetch()
+	},
+	current() {
+		return sessions.findOne(currentSessionId.get())
+	},
+	others() {
+		return sessions.find({
+			_id: { $ne: currentSessionId.get() },
+		}).fetch()
+	},
+	kill(sessionId, callback) {
+		Meteor.call('sessions_kill', sessionId, callback)
+	},
+}
