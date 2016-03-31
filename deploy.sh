557a80190a23eed7e77c31c4c6641e647ba27497
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-03-31 16:30:47 +0200

    deploy.sh: clear dangling images

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 meteor npm install
 docker build -t simplyhomework . && docker-compose up -d app
+ssh simplyhomework 'docker rmi $(docker images -q -f dangling=true)' # reclaim some diskspace
 tput bel
