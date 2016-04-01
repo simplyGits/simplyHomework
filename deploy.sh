c069013251098ce19489a191784c597f6b7dc5d8
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-04-01 16:55:26 +0200

    deploy.sh: show warning if shrinkwrap is possibly out-of-date

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+if [ "npm-shrinkwrap.json" -ot "package.json" ]; then
+	read -p "npm-shrinkwrap.json is older than package.json,
+it's recommended to run \`meteor npm shrinkwrap\` and test if everything works.
+Are you sure you want to continue? [y/N] " response
+	if ! [[ $response =~ ^(yes|y|Y|Yes)$ ]]; then
+		exit 1
+	fi
+fi
 meteor npm install
 docker build -t simplyhomework . && docker-compose up -d app
 ssh simplyhomework 'docker rmi $(docker images -q -f dangling=true)' # reclaim some diskspace
