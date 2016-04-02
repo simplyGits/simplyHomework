ef8c6e3dfc354751023665d0c42e3df006d11087
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-04-02 15:22:51 +0200

    deploy.sh: quit when not on 'master' branch

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+branch=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$branch" != "master" ]]; then
+	>&2 echo "not on 'master' branch"
+	exit 1
+fi
+
 if [ "npm-shrinkwrap.json" -ot "package.json" ]; then
 	read -p "npm-shrinkwrap.json is older than package.json,
-it's recommended to run \`meteor npm shrinkwrap\` and test if everything works.
-Are you sure you want to continue? [y/N] " response
+it's recommended to test if everything works and run \`meteor npm shrinkwrap\`.
+are you sure you want to continue? [y/N] " response
 	if ! [[ $response =~ ^(yes|y|Y|Yes)$ ]]; then
 		exit 1
 	fi
