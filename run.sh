13d9a8de1fa773ff9c9881321952e4077164cdf3
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-05-08 20:57:41 +0200

    run.sh: use local mongodb
    we use mongodb on production, Meteor currently ships with mongodb 2 so we can
    use a local mongo 3 server instead

--- run.sh
+++ run.sh
@@ -8,7 +8,14 @@ fi
 git submodule init
 git submodule update --recursive --remote --merge ./
 meteor npm install
-base="MAIL_URL='smtp://lieuwerooijakkers@gmail.com:3_oSTg-KlPmYICxcGivZMg@smtp.mandrillapp.com:587' DISABLE_WEBSOCKETS=true meteor"
+
+# kill all child processes on exit
+trap 'trap - SIGTERM && kill 0' SIGINT SIGTERM EXIT
+
+echo "Starting mongod"
+mongod --dbpath .meteor/local/db &>/dev/null &
+
+base="MONGO_URL=mongodb://localhost:27017/meteor MAIL_URL='smtp://lieuwerooijakkers@gmail.com:3_oSTg-KlPmYICxcGivZMg@smtp.mandrillapp.com:587' DISABLE_WEBSOCKETS=true meteor"
 if [[ -f settings.json ]]; then
 	eval "$base --settings settings.json"
 else
