9f1329eb050ef45c2b171a89504f06515af62f3f
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-07-26 20:16:53 +0200

    run.sh: custom mongodb isn't needed anymore since c050830df0ac5a968848beabb68323c277c0e15a

--- run.sh
+++ run.sh
@@ -9,13 +9,7 @@ git submodule init
 git submodule update --recursive --remote --merge ./
 meteor npm install
 
-# kill all child processes on exit
-trap 'trap - SIGTERM && kill 0' SIGINT SIGTERM EXIT
-
-echo "Starting mongod"
-mongod --dbpath .meteor/local/db &>/dev/null &
-
-base="MONGO_URL=mongodb://localhost:27017/meteor MAIL_URL='smtp://lieuwerooijakkers@gmail.com:3_oSTg-KlPmYICxcGivZMg@smtp.mandrillapp.com:587' DISABLE_WEBSOCKETS=true meteor $@"
+base="MAIL_URL='smtp://lieuwerooijakkers@gmail.com:3_oSTg-KlPmYICxcGivZMg@smtp.mandrillapp.com:587' DISABLE_WEBSOCKETS=true meteor $@"
 if [[ -f settings.json ]]; then
 	eval "$base --settings settings.json"
 else
