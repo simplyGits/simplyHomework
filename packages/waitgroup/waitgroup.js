8695ea29478759551ed508d2551d16f6a0d61c49
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2017-02-13 16:49:15 +0100

    waitgroup: add `groupSize` param to `map` and `forEach`

--- packages/waitgroup/waitgroup.js
+++ packages/waitgroup/waitgroup.js
@@ -29,21 +29,24 @@ export default class WaitGroup {
 		}
 	}
 
-	static forEach(arr, fn) {
-		const wg = new WaitGroup()
-		arr.forEach(function (x, i) {
-			wg.defer(function () {
-				fn(x, i, arr)
-			})
-		})
-		wg.wait()
+	static forEach(arr, fn, groupSize = Infinity) {
+		for (let g = 0; g < arr.length; g += groupSize) {
+			const wg = new WaitGroup()
+			for (let i = g; i-g<groupSize && i<arr.length; i++) {
+				const x = arr[i]
+				wg.defer(function () {
+					fn(x, i, arr)
+				})
+			}
+			wg.wait()
+		}
 	}
 
-	static map(arr, fn) {
+	static map(arr, fn, groupSize = Infinity) {
 		const res = new Array(arr.length)
-		WaitGroup.forEach(arr, function (x, i) {
-			res[i] = fn()
-		})
+		WaitGroup.forEach(arr, function (x, i, arr) {
+			res[i] = fn(x, i, arr)
+		}, groupSize)
 		return res
 	}
 }
