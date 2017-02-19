af5df311fd208ec28387b9affb5fdc454cbd6f9f
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2017-02-19 18:07:58 +0100

    fix onesignal after 00c91640

--- packages/onesignal/main.js
+++ packages/onesignal/main.js
@@ -1,7 +1,7 @@
 if (!Meteor.settings.public.onesignal) {
 	console.warn('`settings.public.onesignal` is null or undefined, onesignal package will be disabled.');
 } else if (Meteor.isClient) {
-	require('./client.js');
+	module.exports = require('./client.js');
 } else if (Meteor.isServer) {
-	require('./server.js');
+	module.exports = require('./server.js');
 }
